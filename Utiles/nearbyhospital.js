0932caa0424d460c0bc591a7425d36b2b6dffb97
Amr Eldessouki <amreldessoukiad@gmail.com>
2020-05-26 21:33:56 +0200

    html_css

--- Utiles/nearbyhospital.js
+++ Utiles/nearbyhospital.js
@@ -6,11 +6,6 @@ const getdistance = (mylocation,arrlocations)=>
     arrlocations.forEach(element => 
     {
         var distance = Distance.between(mylocation,element.location)
-    
-        
-
-        
-        
         nearby_one = nearby_one.concat({lon:element.location.lon,lat:element.location.lat,distance:distance.human_readable(),name:element.name})
         
         
